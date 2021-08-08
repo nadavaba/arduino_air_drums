b06987b7a8184b6a5db0850f753718985f0bd483
Yarden <yardenwolf@gmail.com>
2021-08-08 12:14:30 +0300

    fade works with code and with the kick problem as well, new flags for is_drum in set

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,6 @@
 #define BRIGHTNESS  255
 #define LED_TYPE    WS2812
 #define COLOR_ORDER GRB
-/*
-#define SNARE_COLOR CRGB::Red
-#define KICK_COLOR CRGB::Yellow
-#define TOM_COLOR CRGB(0xFF,0X80,0X00)
-#define FLOOR_COLOR CRGB::DarkGreen
-#define HIHAT_COLOR CRGB::Blue
-#define RIDE_COLOR CRGB::Magenta
-*/
 
 #define SNARE_COLOR CHSV(0,0,128) //RED
 #define KICK_COLOR CHSV(153,255,128) //YELLOW
@@ -27,11 +19,19 @@
 //number of drums is 6
 #define NUM_LEDS_IN_DRUM 60
 #define NUM_OF_DRUMS 6
+
+//A drum is in the set only if we have a led ring for it
+
+#define IS_SNARE 1
+#define IS_KICK 0
+#define IS_TOM 1
+#define IS_FLOOR 0
+#define IS_HIHAT 1
+#define IS_RIDE 1
+
 enum drum{RIDE, SNARE, TOM, HIHAT, FLOOR, KICK};
 typedef enum drum Drum;
-
 CRGB leds[LED_COUNT];
-
 int start_index[NUM_OF_DRUMS] = {SNARE*NUM_LEDS_IN_DRUM,KICK*NUM_LEDS_IN_DRUM,TOM*NUM_LEDS_IN_DRUM,
                                  FLOOR*NUM_LEDS_IN_DRUM,HIHAT*NUM_LEDS_IN_DRUM,RIDE*NUM_LEDS_IN_DRUM};
 
@@ -40,6 +40,7 @@ int start_index[NUM_OF_DRUMS] = {SNARE*NUM_LEDS_IN_DRUM,KICK*NUM_LEDS_IN_DRUM,TO
 void showDrum(Drum drum, const struct CHSV& color){
     //fill_solid( leds + start_index[drum], NUM_LEDS_IN_DRUM,color);
     fill_solid( leds + drum * NUM_LEDS_IN_DRUM, NUM_LEDS_IN_DRUM,color);
+    FastLED.show();
 }
 
 void fullLedPass(){
@@ -76,35 +77,25 @@ void displayColorByDrum(){
     char drum_name = Serial.read();
     if(drum_name == 's'){
         showDrum(SNARE, SNARE_COLOR);
-        //chosen_color = SNARE_COLOR;
-    } else if (drum_name == 'k'){
+    } else if (IS_KICK && drum_name == 'k'){
         showDrum(KICK, KICK_COLOR);
-        //chosen_color = KICK_COLOR;
-    } else if (drum_name == 't'){
+    } else if (IS_TOM && drum_name == 't'){
         showDrum(TOM, TOM_COLOR);
-        //chosen_color = TOM_COLOR;
-    } else if (drum_name == 'f'){
+    } else if (IS_FLOOR && drum_name == 'f'){
         showDrum(FLOOR, FLOOR_COLOR);
-        //chosen_color = FLOOR_COLOR;
-    } else if (drum_name == 'h'){
+    } else if (IS_HIHAT && drum_name == 'h'){
         showDrum(HIHAT, HIHAT_COLOR);
-        //chosen_color = HIHAT_COLOR;
-    } else if(drum_name == 'r'){
+    } else if(IS_RIDE && drum_name == 'r'){
         showDrum(RIDE, RIDE_COLOR);
-        //chosen_color = RIDE_COLOR;
     }
-    FastLED.show() ;
+    else{
+        Serial.flush();
+    }
 }
 
 void loop() {
-    /*
-    for(int i=0;i<LED_COUNT;i++){
-        leds[i] = CHSV(0,0,0);
-    }
-     */
-    fadeToBlackBy(leds, LED_COUNT, 100);
+    fadeToBlackBy(leds, LED_COUNT, 50);
     FastLED.show();
     displayColorByDrum();
-    FastLED.show();
 }
 
